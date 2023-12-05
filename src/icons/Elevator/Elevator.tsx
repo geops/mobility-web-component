3da11694f45f4f720e74d8dcf1d08697df249116
Olivier Terral <olivier.terral@geops.com>
2023-12-05 16:52:20 +0100

    feat: add icons

---
+++ src/icons/Elevator/Elevator.tsx
@@ -0,0 +1,16 @@
+function Elevator({ ...props }) {
+  return (
+    <svg
+      fill="currentColor"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M2 22h15V2H2zm14-1h-6V3h6zM3 3h6v18H3zm15.625 10h3.75L20.5 16zm3.75-2h-3.75L20.5 8z" />
+    </svg>
+  );
+}
+
+export default Elevator;
