80558a8d98906d51c982a539166b6f3e20b2f594
Olivier Terral <olivier.terral@geops.com>
2023-12-20 13:27:35 +0100

    fix: add useI18n hook

--- src/utils/hooks/useI18n.tsx
+++ src/utils/hooks/useI18n.tsx
@@ -2,14 +2,16 @@
 import { createContext } from "preact";
 import { useContext } from "preact/hooks";
 
-export type UseI18NContextType = {
-  t: (id: string) => string;
+export type I18NContextType = {
+  t: (id: string, templateValues: { [key: string]: string }) => string;
 };
 
-export const I18nContext = createContext({ t: (id: string) => id });
+export const I18nContext = createContext({
+  t: (id, templateValues) => `${id} ${JSON.stringify(templateValues)}`,
+} as I18NContextType);
 
-const useI18n = (): UseI18NContextType => {
-  const context = useContext<UseI18NContextType>(I18nContext);
+const useI18n = (): I18NContextType => {
+  const context = useContext<I18NContextType>(I18nContext);
   if (!context) {
     throw new Error("useI18n must be used within a ContextProvider");
   }
