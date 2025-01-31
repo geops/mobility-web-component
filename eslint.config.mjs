16b80b95ceed4fcad3818e390929baef20442c10
Olivier Terral <olivier.terral@geops.com>
2025-01-31 11:15:44 +0100

    fix: upd mbt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,11 @@ export default [
   ...flat,
   // Not supported for tailwind@4 yet, we use prtttier plugin instead
   // ...tailwind.configs["flat/recommended"],
+  // {
+  //   settings: {
+  //     react: {
+  //       version: "18.3.1",
+  //     },
+  //   },
+  // },
 ];
