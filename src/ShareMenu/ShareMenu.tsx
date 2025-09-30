038fd4751ce18c23415b9701dd6a8418bb10f072
Olivier Terral <olivier.terral@geops.com>
2025-09-30 11:57:02 +0200

    fix: add permalink inout

--- src/ShareMenu/ShareMenu.tsx
+++ src/ShareMenu/ShareMenu.tsx
@@ -18,15 +18,13 @@ function ShareMenu({
   return (
     // eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
     <div className={twMerge(`flex flex-col gap-4 ${className}`)} {...props}>
-      <a
-        className="flex items-center gap-2"
+      <Button
+        className="w-fit"
         href={`mailto:?subject=Karte&body=${window?.location.href}`}
       >
-        <Button>
-          <Email />
-          <span>Email senden</span>
-        </Button>
-      </a>
+        <Email />
+        <span>Email senden</span>
+      </Button>
       <CanvasSaveButton map={map}>
         <Button className={"w-fit"}>
           <Image />
