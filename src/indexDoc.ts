f1fe7d4faebaa564e63a336b71e49cd5741fb10b
Olivier Terral <olivier.terral@geops.com>
2025-10-10 14:22:27 +0200

    docs: improve docs

--- src/indexDoc.ts
+++ src/indexDoc.ts
@@ -1,13 +1,30 @@
 // This file contains what we need to build the documentation in the index.html file
 
 import MobilityMapAttributes from "./MobilityMap/MobilityMapAttributes";
+import MobilityMapEvents from "./MobilityMap/MobilityMapEvents";
 import MobilitySearchAttributes from "./MobilitySearch/MobilitySearchAttributes";
+import MobilitySearchEvents from "./MobilitySearch/MobilitySearchEvents";
 
-if (typeof window !== "undefined") {
-  // @ts-expect-error it's wanted
-  window.MobilityMapAttributes = MobilityMapAttributes;
-  // @ts-expect-error it's wanted
-  window.MobilitySearchAttributes = MobilitySearchAttributes;
+declare global {
+  interface Window {
+    MobilityMapAttributes?: typeof MobilityMapAttributes;
+    MobilityMapEvents?: typeof MobilityMapEvents;
+    MobilitySearchAttributes?: typeof MobilitySearchAttributes;
+    MobilitySearchEvents?: typeof MobilitySearchEvents;
+  }
 }
 
-export default { MobilityMapAttributes, MobilitySearchAttributes };
+if (typeof window !== "undefined") {
+  window.MobilityMapAttributes = MobilityMapAttributes;
+  window.MobilityMapEvents = MobilityMapEvents;
+
+  window.MobilitySearchAttributes = MobilitySearchAttributes;
+  window.MobilitySearchEvents = MobilitySearchEvents;
+}
+
+export default {
+  MobilityMapAttributes,
+  MobilityMapEvents,
+  MobilitySearchAttributes,
+  MobilitySearchEvents,
+};
