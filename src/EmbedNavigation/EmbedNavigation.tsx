72c97052fb2a8ef74c7706ded89cbc3380709bf5
Olivier Terral <olivier.terral@geops.com>
2025-11-20 15:22:35 +0100

    fix: improve embed navigation message

--- src/EmbedNavigation/EmbedNavigation.tsx
+++ src/EmbedNavigation/EmbedNavigation.tsx
@@ -1,18 +1,23 @@
 import { MouseWheelZoom } from "ol/interaction";
 import { useEffect, useState } from "preact/hooks";
+import { twMerge } from "tailwind-merge";
 
+import TouchFingers from "../icons/TouchFingers";
+import useI18n from "../utils/hooks/useI18n";
 import useMapContext from "../utils/hooks/useMapContext";
 
 import DragPanWarning from "./DragPanWarning";
 
-function EmbedNavigation() {
+function EmbedNavigation({ elementClassName }: { elementClassName?: string }) {
+  const { t } = useI18n();
   const { isEmbed, map } = useMapContext();
   const [target, setTarget] = useState(null);
+  const [element, setElement] = useState(null);
 
   useEffect(() => {
     let dragPanWarningControl = null;
 
-    if (!map || !target || !isEmbed) {
+    if (!map || !target || !isEmbed || !element) {
       return;
     }
 
@@ -23,7 +28,10 @@ function EmbedNavigation() {
       }
     });
 
-    dragPanWarningControl = new DragPanWarning({ target: target });
+    dragPanWarningControl = new DragPanWarning({
+      element: element,
+      target: target,
+    });
     map.addControl(dragPanWarningControl);
 
     return () => {
@@ -38,14 +46,28 @@ function EmbedNavigation() {
         }
       });
     };
-  }, [map, target, isEmbed]);
+  }, [map, target, isEmbed, element]);
 
   return (
     <div
       ref={(node) => {
         return setTarget(node);
       }}
-    ></div>
+    >
+      <div
+        className={twMerge(
+          "absolute inset-0 z-100000 flex h-full w-full flex-col items-center justify-center bg-black/80 font-bold text-white",
+          elementClassName,
+          "hidden",
+        )}
+        ref={(node) => {
+          return setElement(node);
+        }}
+      >
+        <TouchFingers height="48" width="48"></TouchFingers>
+        <p>{t("use_2_fingers")}</p>
+      </div>
+    </div>
   );
 }
 
