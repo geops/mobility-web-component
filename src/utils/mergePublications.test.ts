ed2c5829b15a9ac8dea1cb4c8894a1ff3f691d96
oterral <oterral@users.noreply.github.com>
2026-02-27 13:04:39 +0100

    fix: merge similar publications (#33)

---
+++ src/utils/mergePublications.test.ts
@@ -0,0 +1,90 @@
+import { PublicationType } from "mobility-toolbox-js/types";
+import mergePublications from "./mergePublications";
+
+describe("mergePublications", () => {
+  it("should merge publications with same publication windows and textual contents", () => {
+    const publications = [
+      {
+        severity: "NORMAL",
+        severityGroup: "NORMAL",
+        serviceCondition: "DIVERTED",
+        serviceConditionGroup: "CHANGES",
+        publicationWindows: [],
+        publicationStops: [],
+        publicationLines: [
+          {
+            hasIcon: true,
+            category: "DISRUPTION",
+            mot: "BUS",
+            lines: [
+              {
+                name: "510",
+                operatorRef: "SBG",
+              },
+              {
+                name: "510",
+                operatorRef: "SBG",
+              },
+              {
+                name: "510",
+                operatorRef: "SBG",
+              },
+            ],
+          },
+        ],
+        textualContentLarge: null,
+        textualContentMedium: {
+          de: {
+            summary:
+              "Linien 510 und 512: Umleitung und Haltentfall wegen einer Baumaßnahme",
+            reason: "",
+            description:
+              "Zwischen <b>Denzlingen Mattenbühl</b> und <b>Denzlingen Bahnhof</b> kommt es vom <b>26. Februar 2026 ab 08:00 Uhr bis auf Weiteres</b> zu <b>einer Umleitung und Haltausfällen</b> wegen einer Baumaßnahme. Betroffen <b>sind die Linien 510 und 512</b>. Es kommt zu folgenden Änderungen:<br>\n                <b>Umleitung Linie 510</b>: Die Haltestellen Denzlingen Gewerbegebiet bis Denzlingen Mattenbühl werden nicht bedient.  <br><br>Die Änderungen sind in der elektronischen Fahrplanauskunft EFA hinterlegt.",
+            consequence: "",
+            durationText: "",
+            recommendation: "",
+          },
+        },
+      },
+      {
+        severity: "NORMAL",
+        severityGroup: "NORMAL",
+        serviceCondition: "DIVERTED",
+        serviceConditionGroup: "CHANGES",
+        publicationWindows: [],
+        publicationStops: [],
+        publicationLines: [
+          {
+            hasIcon: true,
+            category: "DISRUPTION",
+            mot: "BUS",
+            lines: [
+              {
+                name: "512",
+                operatorRef: "SBG",
+              },
+            ],
+          },
+        ],
+        textualContentLarge: null,
+        textualContentMedium: {
+          de: {
+            summary:
+              "Linien 510 und 512: Umleitung und Haltentfall wegen einer Baumaßnahme",
+            reason: "",
+            description:
+              "Zwischen <b>Denzlingen Mattenbühl</b> und <b>Denzlingen Bahnhof</b> kommt es vom <b>26. Februar 2026 ab 08:00 Uhr bis auf Weiteres</b> zu <b>einer Umleitung und Haltausfällen</b> wegen einer Baumaßnahme. Betroffen <b>sind die Linien 510 und 512</b>. Es kommt zu folgenden Änderungen:<br>\n                <b>Umleitung Linie 510</b>: Die Haltestellen Denzlingen Gewerbegebiet bis Denzlingen Mattenbühl werden nicht bedient.  <br><br>Die Änderungen sind in der elektronischen Fahrplanauskunft EFA hinterlegt.",
+            consequence: "",
+            durationText: "",
+            recommendation: "",
+          },
+        },
+      },
+    ] as PublicationType[];
+    const merged = mergePublications(publications);
+    expect(merged).toHaveLength(1);
+    expect(merged[0].publicationLines).toHaveLength(2);
+    expect(merged[0].publicationLines?.[0].lines[0].name).toBe("510");
+    expect(merged[0].publicationLines?.[1].lines[0].name).toBe("512");
+  });
+});
---
+++ src/utils/mergePublications.ts
@@ -0,0 +1,52 @@
+import { PublicationType } from "mobility-toolbox-js/types";
+
+/**
+ * When imported from external source, some publication have the same publication
+ * windows and the same textualContents but only differs the lines or stations.
+ * In that case we merge them (lines and stops) to avoid duplicates in the display.
+ * TODO fixes this in backend oder import
+ */
+const mergePublications = (
+  publications: PublicationType[] = [],
+): PublicationType[] => {
+  const mergedPublications: PublicationType[] = [];
+  try {
+    publications.forEach((publication) => {
+      const publicationsWindowsStr = JSON.stringify(
+        publication.publicationWindows,
+      );
+      const textualContentStr = JSON.stringify(publication.textualContents);
+
+      // Find a previous identical publication
+      const identicalPublication = mergedPublications.find((p) => {
+        return (
+          p.severity === publication.severity &&
+          p.serviceCondition === publication.serviceCondition &&
+          JSON.stringify(p.publicationWindows) === publicationsWindowsStr &&
+          JSON.stringify(p.textualContents) === textualContentStr
+        );
+      });
+
+      if (identicalPublication) {
+        // We move the lines and stops on the same publication to display them together and avoid duplicates
+        identicalPublication.publicationLines = [
+          ...(identicalPublication.publicationLines || []),
+          ...(publication.publicationLines || []),
+        ];
+        identicalPublication.publicationStops = [
+          ...(identicalPublication.publicationStops || []),
+          ...(publication.publicationStops || []),
+        ];
+        return false;
+      }
+      mergedPublications.push(publication);
+    });
+  } catch (error) {
+    // JSON parsing could failed, we log the error and return the non merged
+    // publications to avoid breaking the display
+    console.error("Error merging publications", error);
+    return publications;
+  }
+  return mergedPublications;
+};
+export default mergePublications;
